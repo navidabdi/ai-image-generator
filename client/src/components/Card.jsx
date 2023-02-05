4633b9e1812011881a37323359c941d366d33965
Nabi <artsanaat@gmail.com>
2023-02-05 09:37:53 +0300

    Added transition effect to the cards.

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,13 +4,13 @@ import { download } from "../assets";
 import { downloadImage } from "../utils";
 
 const Card = ({ _id, name, prompt, photo }) => (
-  <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
+  <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card overflow-hidden">
     <img
       className="w-full h-auto object-cover rounded-xl"
       src={photo}
       alt={prompt}
     />
-    <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
+    <div className="group-hover:translate-y-0 flex-col max-h-[94.5%] absolute translate-y-[110%] bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md transition duration-300">
       <p className="text-white text-sm overflow-y-auto prompt">{prompt}</p>
 
       <div className="mt-5 flex justify-between items-center gap-2">
