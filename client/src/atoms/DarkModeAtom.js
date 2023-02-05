29a94618d486fc820d1673f30f4534fbf300566d
Nabi <artsanaat@gmail.com>
2023-02-05 21:09:19 +0300

    Added Detecting theme.

--- client/src/atoms/DarkModeAtom.js
+++ client/src/atoms/DarkModeAtom.js
@@ -1,6 +1,7 @@
 import { atom } from "recoil";
+import { checkTheme } from "../utils";
 
 export const darkModeAtom = atom({
   key: "darkModeAtom",
-  default: false,
+  default: checkTheme,
 });
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,5 @@
-import FileSaver from 'file-saver';
-import { surpriseMePrompts } from '../constant';
+import FileSaver from "file-saver";
+import { surpriseMePrompts } from "../constant";
 
 export function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
@@ -12,4 +12,15 @@ export function getRandomPrompt(prompt) {
 
 export async function downloadImage(_id, photo) {
   FileSaver.saveAs(photo, `download-${_id}.jpg`);
+}
+
+export function checkTheme() {
+  const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+  if (darkThemeMq.matches) {
+    // Theme set to dark.
+    return true;
+  } else {
+    // Theme set to light.
+    return false;
+  }
 }
